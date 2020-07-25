b797a53979164d4ea5abe19cea99b66bd736557d
Danila Shutov <dcbrwn2@gmail.com>
2020-07-26 01:57:31 +0300

    Remove duplication in event translation code

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,22 @@ use wasm_bindgen::prelude::*;
 
 use sketches::Sketchbook;
 
-use crate::sketches::sketch::{PointerEventData, EventTarget, TickEventData, WheelEventData, WindowResizeData};
-use crate::sketches::sketch::SketchEvent::{PointerDown, PointerMove, PointerUp, Tick, Wheel, WindowResize};
+use crate::sketches::sketch::{
+    PointerEventData,
+    EventTarget,
+    TickEventData,
+    WheelEventData,
+    WindowResizeData,
+    SketchEvent
+};
+use crate::sketches::sketch::SketchEvent::{
+    PointerDown,
+    PointerMove,
+    PointerUp,
+    Tick,
+    Wheel,
+    WindowResize
+};
 
 mod common;
 mod sketches;
@@ -21,6 +35,12 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 thread_local! {
     pub static BOOK: RefCell<Sketchbook> = RefCell::new(Sketchbook::new());
+}
+
+fn dispatch(event: SketchEvent) -> () {
+    BOOK.with(|book| {
+        book.borrow_mut().dispatch(event);
+    })
 }
 
 #[wasm_bindgen]
@@ -34,42 +54,30 @@ pub fn on_load(args: Option<String>, canvas: web_sys::HtmlCanvasElement) -> () {
 
 #[wasm_bindgen]
 pub fn on_tick(time: f64) -> () {
-    BOOK.with(|book| {
-        book.borrow_mut().dispatch(Tick(TickEventData { time }));
-    })
+    dispatch(Tick(TickEventData { time }));
 }
 
 #[wasm_bindgen]
 pub fn on_resize(width: f64, height: f64) -> () {
-    BOOK.with(|book| {
-        book.borrow_mut().dispatch(WindowResize(WindowResizeData { width, height }));
-    })
+    dispatch(WindowResize(WindowResizeData { width, height }));
 }
 
 #[wasm_bindgen]
 pub fn on_pointer_down(button: i8, x: f64, y: f64) -> () {
-    BOOK.with(|book| {
-        book.borrow_mut().dispatch(PointerDown(PointerEventData { button, x, y }));
-    })
+    dispatch(PointerDown(PointerEventData { button, x, y }));
 }
 
 #[wasm_bindgen]
 pub fn on_pointer_move(button: i8, x: f64, y: f64) -> () {
-    BOOK.with(|book| {
-        book.borrow_mut().dispatch(PointerMove(PointerEventData { button, x, y }));
-    })
+    dispatch(PointerMove(PointerEventData { button, x, y }));
 }
 
 #[wasm_bindgen]
 pub fn on_pointer_up(button: i8, x: f64, y: f64) -> () {
-    BOOK.with(|book| {
-        book.borrow_mut().dispatch(PointerUp(PointerEventData { button, x, y }));
-    })
+    dispatch(PointerUp(PointerEventData { button, x, y }));
 }
 
 #[wasm_bindgen]
 pub fn on_wheel(dx: f64, dy: f64, px: f64, py: f64) -> () {
-    BOOK.with(|book| {
-        book.borrow_mut().dispatch(Wheel(WheelEventData { dx, dy, px, py }));
-    })
+    dispatch(Wheel(WheelEventData { dx, dy, px, py }));
 }
